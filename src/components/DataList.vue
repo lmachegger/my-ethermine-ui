163c6aba154ca10d951f139b023a32cf66c22368
lmachegger <lmachegger@gmail.com>
2021-04-02 20:37:14 +0200

    removed rounding logic from DataList component

--- src/components/DataList.vue
+++ src/components/DataList.vue
@@ -3,34 +3,18 @@
     <h1 class="dataListHeading">{{ headingText }}</h1>
     <DataListItem
       name="MH/s reported:"
-      :value="roundTo2(data?.reportedHashrate)"
+      :value="data?.reportedHashrate"
       :precision="2"
     />
     <DataListItem
       name="MH/s received:"
-      :value="roundTo2(data?.currentHashrate)"
+      :value="data?.currentHashrate"
       :precision="2"
     />
-    <DataListItem
-      name="Shares/h:"
-      :value="roundTo2(data?.validShares)"
-      :precision="2"
-    />
-    <DataListItem
-      name="ETH/h:"
-      :value="roundTo8(data?.coinsPerHour)"
-      :precision="8"
-    />
-    <DataListItem
-      name="BTC/h:"
-      :value="roundTo8(data?.btcPerHour)"
-      :precision="8"
-    />
-    <DataListItem
-      name="USD/h:"
-      :value="roundTo8(data?.usdPerHour)"
-      :precision="8"
-    />
+    <DataListItem name="Shares/h:" :value="data?.validShares" :precision="2" />
+    <DataListItem name="ETH/h:" :value="data?.coinsPerHour" :precision="8" />
+    <DataListItem name="BTC/h:" :value="data?.btcPerHour" :precision="8" />
+    <DataListItem name="USD/h:" :value="data?.usdPerHour" :precision="8" />
   </div>
 </template>
 <script>
@@ -43,22 +27,6 @@ export default {
   props: {
     data: Object,
     headingText: String,
-  },
-  methods: {
-    roundTo2: function (val) {
-      if (val) {
-        return Math.round(val * 100) / 100;
-      } else {
-        return 0;
-      }
-    },
-    roundTo8: function (val) {
-      if (val) {
-        return Math.round(val * 100000000) / 100000000;
-      } else {
-        return 0;
-      }
-    },
   },
 };
 </script>
